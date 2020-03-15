3a6a642b00942f7d0d223b6bebdf6ae77fdc83d7
LambdAurora <aurora42lambda@gmail.com>
2020-03-15 20:24:53 +0100

    :bookmark: λcommon.js v1.1.2: Fix stupid bug.

--- test/test.mjs
+++ test/test.mjs
@@ -4,10 +4,10 @@ console.log('Testing λcommon.js...');
 const ID_TEST = new Identifier("lambdacommon", "test")
 let result = new TestUnit()
   .section("lambdacommon:identifier", section => {
-    section.add("Identifier(String identifier)", () => new Identifier("lambdacommon:identifier").to_string(), "lambdacommon:identifier");
+    section.add("Identifier(String identifier)", () => new Identifier("lambdacommon:identifier").toString(), "lambdacommon:identifier");
     section.add("Identifier(String identifier, String sub_path)", 
-      () => new Identifier("lambdacommon:identifier", "yay").to_string(), "lambdacommon:identifier/yay");
-    section.add("Identifier.sub(String path)", () => ID_TEST.sub("yay").to_string(), "lambdacommon:test/yay");
+      () => new Identifier("lambdacommon:identifier", "yay").toString(), "lambdacommon:identifier/yay");
+    section.add("Identifier.sub(String path)", () => ID_TEST.sub("yay").toString(), "lambdacommon:test/yay");
     section.add("Identifier.equals(other)", () => ID_TEST.equals("lambdacommon:test"), true);
     section.add("Identifier.equals(other)", () => ID_TEST.equals(ID_TEST), true);
     section.add("Identifier.equals(other)", () => ID_TEST.equals(new Identifier("lambdacommon:identifier")), false);
